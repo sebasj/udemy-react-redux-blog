fb07111d8c077e20ca91b395d95c705fb5e7c8d9
Sebastian Bustamante <sebasj@gmail.com>
2017-03-25 10:36:16 +0100

    fin leccion 83

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPostsAction } from '../actions/index'
+import { Link } from 'react-router';
 
 
 class PostsIndex extends Component {
@@ -10,7 +11,15 @@ class PostsIndex extends Component {
 
     render() {
         return (
-            <div>List of blog posts</div>
+            <div>
+                <div className="text-xs-right">
+                    <Link to="/posts/new" className="btn btn-primary">
+                    Add a Post
+                    </Link>
+
+                </div>
+                List of blog posts
+            </div>
         );
     }
 }
