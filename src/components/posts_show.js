0992f10b3437a29768f710474951f41dab1e9c24
Sebastian Bustamante <sebasj@gmail.com>
2017-04-01 20:28:37 +0200

    fin leccion 94

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -9,9 +9,23 @@ class PostsShow extends Component {
     }
 
     render() {
-        return <div>Show post {this.props.params.id}</div>;
+        console.log(this.props);
+        const selected_post = this.props.selected_post;
+        if (!selected_post) {
+            return <div>Loading...</div>;
+        }
+        return (
+            <div>
+                <h3>{selected_post.title}</h3>
+                <h6>Categories: {selected_post.categories}</h6>
+                <p>Categories: {selected_post.content}</p>
+            </div>
+        );
     }
 }
 
+function mapStateToProps(state) {
+    return { selected_post: state.postsReducer.selected_post }
+}
 
-export default connect(null, { fetchPostAction })(PostsShow);
+export default connect(mapStateToProps, { fetchPostAction })(PostsShow);
