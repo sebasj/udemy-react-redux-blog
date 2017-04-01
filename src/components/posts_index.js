c7b24388eedd659bed40b0d40654edc86152f6c7
Sebastian Bustamante <sebasj@gmail.com>
2017-04-01 19:54:58 +0200

    fin leccion 91

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -13,8 +13,10 @@ class PostsIndex extends Component {
         return this.props.allPosts.map((post) => {
             return (
                 <li className="list-group-item" key={post.id}>
-                    <span className="pull-xs-right">{post.categories}</span>
-                    <strong>{post.title}</strong>
+                    <Link to={"posts/" + post.id}>
+                        <span className="pull-xs-right">{post.categories}</span>
+                        <strong>{post.title}</strong>
+                    </Link>
                 </li>
             )
         });
@@ -24,10 +26,7 @@ class PostsIndex extends Component {
         return (
             <div>
                 <div className="text-xs-right">
-                    <Link to="/posts/new" className="btn btn-primary">
-                    Add a Post
-                    </Link>
-
+                    <Link to="/posts/new" className="btn btn-primary">Add a Post</Link>
                 </div>
                 <h3>Posts</h3>
                 <ul className="list-group">
